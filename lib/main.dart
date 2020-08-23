0f309f7f91c9c25403eeb43f82189d324a8aefdd
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-08-23 14:39:24 +0900

    #2 시작/일시정지 버튼 영역 UI 작성 #3 타이머 영역 UI 작성

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,76 @@ class StopWatchPage extends StatefulWidget {
 class _StopWatchPageState extends State<StopWatchPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('StopWatch'),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomAppBar(
+        child:Container(
+          height:50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState((){
+          _clickBotton();
+        }),
+        child:Icon(Icons.play_arrow),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
+}
+Widget _buildBody(){
+  return Center(
+    child:Padding(
+      padding: const EdgeInsets.only(top:30),
+      child:Stack(
+        children: <Widget>[
+          Column(
+            children:<Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children:<Widget>[
+                  Text(
+                    '0',
+                    style:TextStyle(fontSize: 50.0),
+                  ),
+                  Text('00'),
+                ],
+              ),
+              Container(
+                width:100,
+                height:200,
+                child:ListView(
+                  children:<Widget>[],
+                ),
+              )
+            ],
+          ),
+          Positioned(
+            left:10,
+            bottom:10,
+            child:FloatingActionButton(
+              backgroundColor:Colors.deepOrange,
+              onPressed:(){},
+              child:Icon(Icons.rotate_left),
+            ),
+          ),
+          Positioned(
+            right:10,
+            bottom:10,
+            child:RaisedButton(
+              onPressed:(){},
+              child:Text('랩타임'),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+class _clickBotton {
 }
